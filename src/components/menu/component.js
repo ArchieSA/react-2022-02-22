b798fdaf4e5e847c40b0034ad026e205d5d348cd
Ekaterina Driga <ekaterina.driga@t-systems.com>
2022-03-07 22:45:09 +0300

    chore: minor component improvements

--- src/components/menu/component.js
+++ src/components/menu/component.js
@@ -4,9 +4,18 @@ export const Menu = ({ menu }) => {
   return (
     <div>
       <h2 style={{ textAlign: 'center' }}>Menu</h2>
-      {menu.map(({ id, name, ingredients, price }) => (
-        <Product key={id} name={name} ingredients={ingredients} price={price} />
-      ))}
+      {menu.length > 0 ? (
+        menu.map(({ id, name, ingredients, price }) => (
+          <Product
+            key={id}
+            name={name}
+            ingredients={ingredients}
+            price={price}
+          />
+        ))
+      ) : (
+        <p style={{ margin: '10px' }}>Coming soon...</p>
+      )}
     </div>
   );
 };
--- src/components/reviews/component.js
+++ src/components/reviews/component.js
@@ -4,9 +4,13 @@ export const Reviews = ({ reviews }) => {
   return (
     <div>
       <h2 style={{ textAlign: 'center' }}>Reviews</h2>
-      {reviews.map(({ id, rating, user, text }) => (
-        <Review key={id} user={user} rating={rating} text={text} />
-      ))}
+      {reviews.length > 0 ? (
+        reviews.map(({ id, rating, user, text }) => (
+          <Review key={id} user={user} rating={rating} text={text} />
+        ))
+      ) : (
+        <p style={{ margin: '10px' }}>No reviews</p>
+      )}
     </div>
   );
 };
