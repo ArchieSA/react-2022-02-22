5c0c201e4f496892a1504c16b3f259f283acfd34
a.popova9 <a.popova9@g.nsu.ru>
2022-03-22 01:47:23 +0700

    Homework 4: user store

---
+++ src/components/user/component.jsx
@@ -0,0 +1,5 @@
+export const User = ({ userName, userGender, birthYear }) => {
+    return <div>
+        Hello, {userName}! Your gender is: {userGender}. Your birth year is: {birthYear}.
+    </div>
+};
