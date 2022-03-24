e9d826d40051e0825ec1539236c356acec82dffa
a.popova9 <a.popova9@g.nsu.ru>
2022-03-25 01:11:54 +0700

    Homework 5: add reviews and users reducers

--- src/modules/reducers/index.js
+++ src/modules/reducers/index.js
@@ -2,9 +2,13 @@ import { combineReducers } from 'redux';
 import { basketReducer } from './basket-reducer';
 import { restaurantReducer } from './restaurant-reducers';
 import { productReducer } from './product-reducers';
+import { reviewReducer } from './review-reducer';
+import { userReducer } from './user-reducer';
 
 export const rootReducer = combineReducers({
     basket: basketReducer,
     restaurants: restaurantReducer,
     products: productReducer,
+    reviews: reviewReducer,
+    users: userReducer
 });
